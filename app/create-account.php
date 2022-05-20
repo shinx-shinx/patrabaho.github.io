9b0accbb86f1e0a844a826e4fb644c26a1b524c7
romar <romardelacruz29@gmail.com>
2022-05-20 13:02:34 +0800

    update

--- app/create-account.php
+++ app/create-account.php
@@ -81,62 +81,91 @@ function register() {
 
         $stmt->execute();
 
-        $roles = json_decode($role);
-
-        session_start();
-        $_SESSION['logged'] = true;
-        foreach($roles as $myrole) {
-            if($myrole == 'employee'){
-                $_SESSION['myid'] = $row['member_no'];
-                $_SESSION['myfname'] = $row['first_name'];
-                $_SESSION['mylname'] = $row['last_name'];
-                $_SESSION['myemail'] = $row['email'];
-                $_SESSION['mydate'] = $row['bdate'];
-                $_SESSION['mymonth'] = $row['bmonth'];
-                $_SESSION['myyear'] = $row['byear'];
-                $_SESSION['myphone'] = $row['phone'];
-                $_SESSION['myedu'] = $row['education'];
-                $_SESSION['mytitle'] = $row['title'];
-                $_SESSION['mycity'] = $row['city'];
-                $_SESSION['mystreet'] = $row['street'];
-                $_SESSION['myzip'] = $row['zip'];
-                $_SESSION['mybarangay'] = $row['barangay'];
-                $_SESSION['mydesc'] = $row['about'];
-
-
-                $_SESSION['avatar'] = $row['avatar'];
-                $_SESSION['lastlogin'] = $last_login;
-                $_SESSION['avatar'] = $row['avatar'];
-                $_SESSION['gender'] = $row['avatar'];
-                $_SESSION['employee'] = true;
-            }
-            else {
-				$_SESSION['myid'] = $row['member_no'];
-				$_SESSION['compname'] = $row['company'];
-				$_SESSION['established'] = $row['byear'];
-				$_SESSION['myemail'] = $row['email'];
-				$_SESSION['myphone'] = $row['phone'];
-				$_SESSION['comptype'] = $row['title'];
-				$_SESSION['mycity'] = $row['city'];
-				$_SESSION['mystreet'] = $row['street'];
-				$_SESSION['myzip'] = $row['zip'];
-				$_SESSION['mybarangay'] = $row['barangay'];
-				$_SESSION['mydesc'] = $row['about'];
-				$_SESSION['avatar'] = $row['avatar'];
-				$_SESSION['myserv'] = $row['services'];
-				$_SESSION['myexp'] = $row['expertise'];
-				$_SESSION['lastlogin'] = $last_login;
-				$_SESSION['website'] = $row['website'];
-				$_SESSION['people'] = $row['people'];
-				$_SESSION['employer'] = $true;
-            }
-        }
-        $_SESSION['role'] = 'employee';
+        authenticate($email, $login, $conn, $last_login);
         header("location:../employee");
     }
     catch(PDOException $e)
     {
         echo $e;
+    }
+}
+
+function authenticate($myemail, $mypass, $conn) {
+    try {
+		$stmt = $conn->prepare("SELECT * FROM tbl_users WHERE email = :myemail AND login = :mypassword");
+		$stmt->bindParam(':myemail', $myemail);
+		$stmt->bindParam(':mypassword', $mypass);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+		$rec = count($result);
+		
+		if ($rec == "0") {
+			header("location:../login.php?r=0346");
+		}else{
+
+		foreach($result as $row)
+		{
+			$role = $row['role'];
+			$roles = json_decode($role);
+
+			session_start();
+			$_SESSION['logged'] = true;
+			foreach($roles as $myrole) {
+				if($myrole == 'employee'){
+					$_SESSION['myid'] = $row['member_no'];
+					$_SESSION['myfname'] = $row['first_name'];
+					$_SESSION['mylname'] = $row['last_name'];
+					$_SESSION['myemail'] = $row['email'];
+					$_SESSION['mydate'] = $row['bdate'];
+					$_SESSION['mymonth'] = $row['bmonth'];
+					$_SESSION['myyear'] = $row['byear'];
+					$_SESSION['myphone'] = $row['phone'];
+					$_SESSION['myedu'] = $row['education'];
+					$_SESSION['mytitle'] = $row['title'];
+					$_SESSION['mycity'] = $row['city'];
+					$_SESSION['mystreet'] = $row['street'];
+					$_SESSION['myzip'] = $row['zip'];
+					$_SESSION['mybarangay'] = $row['barangay'];
+					$_SESSION['mydesc'] = $row['about'];
+
+
+					$_SESSION['avatar'] = $row['avatar'];
+					$_SESSION['lastlogin'] = $last_login;
+					$_SESSION['avatar'] = $row['avatar'];
+					$_SESSION['gender'] = $row['avatar'];
+					$_SESSION['employee'] = true;
+				}
+				else {
+					$_SESSION['myid'] = $row['member_no'];
+					$_SESSION['compname'] = $row['company'];
+					$_SESSION['established'] = $row['byear'];
+					$_SESSION['myemail'] = $row['email'];
+					$_SESSION['myphone'] = $row['phone'];
+					$_SESSION['comptype'] = $row['title'];
+					$_SESSION['mycity'] = $row['city'];
+					$_SESSION['mystreet'] = $row['street'];
+					$_SESSION['myzip'] = $row['zip'];
+					$_SESSION['mybarangay'] = $row['barangay'];
+					$_SESSION['mydesc'] = $row['about'];
+					$_SESSION['avatar'] = $row['avatar'];
+					$_SESSION['myserv'] = $row['services'];
+					$_SESSION['myexp'] = $row['expertise'];
+					$_SESSION['lastlogin'] = $last_login;
+					$_SESSION['website'] = $row['website'];
+					$_SESSION['people'] = $row['people'];
+					$_SESSION['employer'] = true;
+				}
+			}
+			$_SESSION['role'] = 'employee';	
+
+		}
+	
+	}
+
+					  
+	}catch(PDOException $e)
+    {
+
     }
 }
 // function register_as_employee() {
